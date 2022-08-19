5c1cd0934f0d2464328a216809714493c4dfa2ea
furkan <furkansahin824@gmail.com>
2022-08-18 23:45:58 -0500

    remove PATHS

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,9 +11,6 @@ const config = {
     adapter: adapter(),
     prerender: {
       default: true
-    },
-    paths: {
-      base: dev ? '' : '/testing-svelte-gh'
     }
   }
 };
